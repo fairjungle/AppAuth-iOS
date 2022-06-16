7ce16181d1825424e54aebcd572674c99a63ea74
Gwendal Roué <gr@pierlis.com>
2022-06-16 11:42:52 +0200

    Support for NSSecureCoding
    Fix for https://github.com/openid/AppAuth-iOS/issues/479
    
    Plea for an upstream fix: https://github.com/openid/AppAuth-iOS/issues/479#issuecomment-1157467641

--- Source/AppAuthCore/OIDServiceConfiguration.m
+++ Source/AppAuthCore/OIDServiceConfiguration.m
@@ -185,7 +185,12 @@ NS_ASSUME_NONNULL_BEGIN
     return nil;
   }
 
-  OIDServiceDiscovery *discoveryDocument = [aDecoder decodeObjectOfClass:[OIDServiceDiscovery class]
+  OIDServiceDiscovery *discoveryDocument = [aDecoder decodeObjectOfClasses:[NSSet setWithObjects:
+                                                                            [OIDServiceDiscovery class],
+                                                                            [NSArray class],
+                                                                            [NSString class],
+                                                                            [NSNumber class],
+                                                                            nil]
                                                                   forKey:kDiscoveryDocumentKey];
 
   return [self initWithAuthorizationEndpoint:authorizationEndpoint
